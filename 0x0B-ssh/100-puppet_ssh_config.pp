c6f83ccb2a11f1368acd88eb030b014789f06164
Oluwatobi <amure387@gmail.com>
2023-03-25 14:08:03 +0000

    feat: used puppet to edit config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # configure ssh with puppet
+include stdlib
 file_line {'Refuse auth':
 ensure => present,
-path => '/etc/ssh/sshd_config',
-line => '  PasswordAuthentication no',
+path   => '/etc/ssh/sshd_config',
+line   => '  PasswordAuthentication no',
 }
 
 file_line {'Specifies private key':
 ensure => present,
-path => '/etc/ssh/sshd_config',
-line => '  IdentityFile ~/.ssh/school',
+path   => '/etc/ssh/sshd_config',
+line   => '  IdentityFile ~/.ssh/school',
 }
