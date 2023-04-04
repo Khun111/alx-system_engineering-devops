14dcdc3c494dc41f24d7752c9bfa24943f069fad
Oluwatobi <amure387@gmail.com>
2023-04-04 03:16:15 +0000

    Feat: custom header with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,16 @@
-# Create Cusmtom headee with puppet
+# Create Custom header with puppet
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file_line { 'custom_header':
+  path => '/etc/nginx/sites-enabled/default',
+  line => '    add_header X-Served-By \$hostname',
+  match => '^    listen 80 default_server;$',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enabled => 'true'
+}
