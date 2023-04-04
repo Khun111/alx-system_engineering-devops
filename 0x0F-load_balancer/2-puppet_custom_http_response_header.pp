f808b58b9c4189a57df101bca6b8a18a225fb628
Oluwatobi <amure387@gmail.com>
2023-04-04 03:27:58 +0000

    fix: task 1 and 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,7 +5,7 @@ package { 'nginx':
 
 file_line { 'custom_header':
   path => '/etc/nginx/sites-enabled/default',
-  line => '    add_header X-Served-By \$hostname',
+  line => '    add_header X-Served-By $hostname;',
   match => '^    listen 80 default_server;$',
   require => Package['nginx'],
 }
