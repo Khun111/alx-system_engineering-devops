bd2e4811ee4d536161421977fe9643495ecb4701
Oluwatobi <amure387@gmail.com>
2023-04-24 22:24:45 +0000

    fix: made task 0 executable, feat: used csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"give information about todo progress"
+'''use csv format'''
 import requests
 import sys
 
@@ -15,20 +15,16 @@ if __name__ == '__main__':
     t_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
         .format(employee_id)
     response = requests.get(t_url)
-    employee = requests.get(employee_url).json()
+    todos, employee = response.json(), requests.get(employee_url).json()
 
     if response.status_code != 200:
         print("Employee with ID {} not found".format(employee_id))
         sys.exit(1)
-    todos = response.json()
 
-    completed = [todo for todo in todos if todo['completed']]
-    employee_name = employee.get('name')
-    total_tasks = len(todos)
-    len_completed = len(completed)
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, len_completed, total_tasks))
-
-    for task in completed:
-        print("\t {}".format(task['title']))
+    csv_f = '{}.csv'.format(employee_id)
+    with open(csv_f, 'w') as file:
+        for task in todos:
+            formatw = '"{}", "{}", "{}", "{}"\n'.format(
+                employee_id, employee['username'],
+                task['completed'], task['title'])
+            file.write(formatw)
