8b6e59da580950b5fa45e50a9a0f368ed85e2fa9
Oluwatobi <amure387@gmail.com>
2023-04-25 04:59:28 +0000

    pish

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 '''use json format'''
 import json
 import requests
@@ -6,23 +7,21 @@ import sys
 
 if __name__ == '__main__':
     '''Main task with api'''
-    t_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'
-    response = requests.get(t_url.format(1))
+    t_url = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(t_url)
     todos = response.json()
     employees = {}
 
     for task in todos:
         employee_id = task.get('userId')
+        if employee_id not in employees:
+            employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
+            employees[employee_id] = {'username': employee.get('username'), 'tasks': []}
         todo = {
-            'username': '',
             'task': task.get('title'),
             'completed': task.get('completed')
         }
-        if employee_id not in employees:
-            employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
-            employees[employee_id] = [{'username': employee.get('username'), 'task': task.get('title'), 'completed': task.get('completed')}]
-        else:
-            employees[employee_id].append({'username': employee.get('username'), 'task': task.get('title'), 'completed': task.get('completed')})
+        employees[employee_id]['tasks'].append(todo)
 
     json_f = 'todo_all_employees.json'
     with open(json_f, 'w') as file:
