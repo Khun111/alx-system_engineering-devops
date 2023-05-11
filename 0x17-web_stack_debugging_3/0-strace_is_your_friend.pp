122140fa2cf125d06d7999b0157de931d8023c6b
Oluwatobi <amure387@gmail.com>
2023-05-11 21:23:52 +0000

    fix: added path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Puppet file for editing wp config file
 exec { 'replace_content':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }
