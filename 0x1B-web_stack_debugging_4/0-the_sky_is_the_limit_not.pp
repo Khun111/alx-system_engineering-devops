651cdc74c15965ee24b883bbf36dfd161452fb57
Oluwatobi <amure387@gmail.com>
2023-05-18 21:57:07 +0000

    feat:added puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,10 @@
-#Increase ULIMIT                                exec { 'fix--for-nginx':                          command => "sed -i -E 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
-  path    => 'usr/local/bin/:/bin',             }
-                                                service { 'nginx':
-  ensure   => running,
-  enable   => true,
+#Increase ULIMIT
+exec { 'fix--for-nginx':
+  command => "sed -i -E 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
+  path    => 'usr/local/bin/:/bin',
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
